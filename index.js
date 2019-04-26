af50c08a43d5878a3c1d53c322a9122cb818687d
afifalfiano <afifalfiano2@gmail.com>
2019-04-26 14:52:01 +0700

    Basicly Vue Js

--- index.js
+++ index.js
@@ -4,3 +4,42 @@
       message: 'Hello Vue!'
     }
  });
+
+ let app2 = new Vue({
+   el: '#app-2',
+   data: {
+     message: 'You loaded this page on' + new Date().toLocaleString()
+
+   }
+ });
+
+ let app3 = new Vue({
+   el: '#app-3',
+   data: {
+     seen:true
+   }
+
+ });
+
+ let app4 = new Vue({
+   el: '#app-4',
+   data: {
+     todos: [
+       { text: 'Learn Javascript' },
+       { text: 'Learn Vue' },
+       { text: 'Build Something Awesome'}
+     ]
+   }
+ })
+
+ let app5 = new Vue({
+   el: '#app-5',
+   data: {
+     message: 'Hello Vue Js!'
+   },
+   methods: {
+     reverseMessage: function() {
+       this.message = this.message.split('').reverse().join()
+     }
+   }
+ })
